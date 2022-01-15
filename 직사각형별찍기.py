73790c2eac8df358faa9063f0584d2503eff0d31
jobumjin <whqjawls2673@naver.com>
2022-01-15 23:30:53 +0900

    Programmers

---
+++ 직사각형별찍기.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().strip().split(' '))
+for i in range(b):
+    print(a*"*")
+
+
+## 다른사람 풀이
+a, b = map(int, input().strip().split(' '))
+answer = ('*'*a +'\n')*b
+print(answer)
